37791a1a37f7f79c98d1603c639e72b6f6841f66
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-10-26 18:25:18 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,7 +1,22 @@
 package de.demmer.dennis;
 
 public class Application {
+
+
+	public static void main(String[] args) {
+		
+		int i = addieren(5,8);
+		System.out.println("Das Ergebnis ist " + i);
+		
+	}
 	
+	
+	
+	static int addieren(int zahl1, int zahl2) {
+		int ergebnis = zahl1 + zahl2;
+		System.out.println(ergebnis);
+		return ergebnis;
+	}
 	
 
 }
