d1115b9f53256c749519fd4f9bba65f55ba184c6
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-10-26 19:17:40 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -2,21 +2,50 @@ package de.demmer.dennis;
 
 public class Application {
 
-
 	public static void main(String[] args) {
-		
-		int i = addieren(5,8);
+
+		int i = addieren(5, 8);
 		System.out.println("Das Ergebnis ist " + i);
 		
+		
+		boolean b = gleichheit(8,8);
+		System.out.println(b);
+		
+		
+		double d = durchZwei(13);
+		System.out.println(d);
+		
+		
+		dreimalKonsole("Hallo Welt");
+		
 	}
-	
-	
-	
+
+	// Die Methode hat zwei Zahlen als Argumente die addiert werden sollen
 	static int addieren(int zahl1, int zahl2) {
 		int ergebnis = zahl1 + zahl2;
-		System.out.println(ergebnis);
 		return ergebnis;
 	}
+
+	
+	//    | return |  name   |        arguments   |
+	static boolean gleichheit(int zahl1, int zahl2) {
+		boolean b = zahl1 == zahl2;
+		return b;
+	}
+	
+	
+	static double durchZwei(int zahl) {
+		return zahl /2D;
+	}
+	
+	
+	static void dreimalKonsole(String s) {
+		System.out.println(s);
+		System.out.println(s);
+		System.out.println(s);
+	}
+	
+
 	
 
 }
